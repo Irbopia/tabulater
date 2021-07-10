da9423184a7844287013e1802a8c75a0321d5950
irbopia <travislockett1@gmail.com>
2021-07-10 15:37:00 -0400

    Final

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+
+
 // chrome.browserAction.onClicked.addListener(function (activetab) {
 // 	chrome.tabs.create({ 'url': chrome.extension.getURL('tabulater.html') }, function (tab));
 // });
@@ -7,18 +9,20 @@
 // });
 
 chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
-    document.write(`<link rel="stylesheet" href="style.css">`);
-    document.write(`<h3>Tab List</h3>`);
+    document.write(`<link rel="stylesheet" href="style.css">`)
+    document.write(`<h3>All open tabs</h3>`);
+    document.write(`You have <strong>${tabs.length}</strong> tabs open <br /><br />`);
+    document.write(`<a href="tabulater.html" target="_blank" id="close_tabs">Close Tabs</a>`);
     document.write('<ul>');
     for(let i = 0; i < tabs.length; i++) {
         document.write(`<li><a href="${tabs[i].url}" target="_blank">${tabs[i].url}</a></li>`);
     }
 	document.write('<ul>');
 
-	// loop through and remove all active tabs besides the current open tab
-    for(let i = 0; i < tabs.length-1; i++){
-        chrome.tabs.remove(tabs[i].id);
-    }
+	// loop through and remove all active tabs
+    // for(let i = 0; i < tabs.length-1; i++){
+    //     chrome.tabs.remove(tabs[i].id);
+    // }
 	
 });
 
