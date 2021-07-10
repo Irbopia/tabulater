d7ebf2c990698eadfa15b3899dc42641613a9f37
irbopia <travislockett1@gmail.com>
2021-07-10 12:58:44 -0400

    Saturday progress

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
-//chrome.tabs.create(object createProperties, function callback)
+// chrome.browserAction.onClicked.addListener(function (activetab) {
+// 	chrome.tabs.create({ 'url': chrome.extension.getURL('tabulater.html') }, function (tab));
+// });
 
-chrome.tabs.create({ url: chrome.extension.getURL ("tabulater.html")}, () => {console.log("Turtles")})
+// chrome.browserAction.onClicked.addListener(function () {
+// 	chrome.tabs.create({ url: chrome.runtime.getURL("tabulater.html") });
+// });
+
+chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
+    document.write(`<h3>The tabs you're on are:</h3>`);
+    document.write('<ul>');
+    for(let i = 0; i < tabs.length; i++) {
+        document.write(`<li>${tabs[i].url}</li>`);
+    }
+	document.write('<ul>');
+
+	// loop through and remove all active tabs
+    for(let i = 0; i < tabs.length-1; i++){
+        chrome.tabs.remove(tabs[i].id);
+    }
+	
+});
+
+
+
+
+
+
+
+
+// chrome.browserAction.onClicked.addListener(function(activeTab){
+//   chrome.tabs.create({'url': chrome.extension.getURL('notes.html')}, function(tab)
+//   });
+// });
